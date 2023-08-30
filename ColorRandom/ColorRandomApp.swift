7dadf25b268bb39e1015955a1ae23c7b9298f5ae
AIWASS23 <marcelodearaujo677@gmail.com>
2023-08-30 11:31:43 -0300

    Initial Commit

---
+++ ColorRandom/ColorRandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorRandomApp.swift
+//  ColorRandom
+//
+//  Created by Marcelo De Araújo on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorRandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
